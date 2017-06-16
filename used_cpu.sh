1de9752246ddcc00c9c39bda27c2223dddf8647a
JM Manalo <manalojuanmiguel@rocketmail.com>
2017-06-16 11:39:56 +0800

    Machine Exercise 1

---
+++ used_cpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+while true
+do
+	D=$(date +'%Y-%m-%d_%H')
+	DN=$(date +'%Y-%m-%d')
+	echo -e `date +'%F %T'`'\t' `top | head -n3 | tail -n1 | tr -d ',' | awk '{print $2+$4}'` >> cpu_util_$D.txt
+	ls -Rrt > file.txt
+	grep 'cpu_util_' file.txt | while read- r line; do
+		Time=$(echo $line | head -c 22 | tail -c 13)
+		DP=$(echo $line | head -c 19 | tail -c 10)
+		diff=$(($(($(date -d $DN +%s) - $(date -d $DP +%s)))/3600))
+		if [ $diff -ge 168 ]; then
+			rm $line
+		else
+			if [ $Time != $D ]; then
+				gzip -q $line
+			fi
+		fi
+	done
+	rm file.txt
+	sleep 5
+done
