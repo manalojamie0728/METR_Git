a38a4b3652a0ea0ec38b245f32f351c6823d249e
JM Manalo <manalojuanmiguel@rocketmail.com>
2017-06-16 13:53:21 +0800

    Further refinements made with regards to top command

--- used_cpu.sh
+++ used_cpu.sh
@@ -3,7 +3,7 @@ while true
 do
 	D=$(date +'%Y-%m-%d_%H')
 	DN=$(date +'%Y-%m-%d')
-	echo -e `date +'%F %T'`'\t' `top | head -n3 | tail -n1 | tr -d ',' | awk '{print $2+$4}'` >> cpu_util_$D.txt
+	echo -e `date +'%F %T'`'\t' `top -b -n2 -d5 | head -n3 | tail -n1 | tr -d ',' | awk '{print $2+$4}'` >> cpu_util_$D.txt
 	find cpu_util_* > file.txt | sed 's/ /\n/g'
 	while read- r line; do
 		Time=$(echo $line | head -c 22 | tail -c 13)
@@ -16,7 +16,7 @@ do
 				gzip -q $line
 			fi
 		fi
-	done
+	done < file.txt
 	rm file.txt
 	sleep 5
 done
