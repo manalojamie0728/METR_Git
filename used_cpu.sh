a944a3bf8d8908db2eb76cd3d9312861c4ec257e
JM Manalo <manalojuanmiguel@rocketmail.com>
2017-06-16 12:44:51 +0800

    Refined algorithm

--- used_cpu.sh
+++ used_cpu.sh
@@ -4,8 +4,8 @@ do
 	D=$(date +'%Y-%m-%d_%H')
 	DN=$(date +'%Y-%m-%d')
 	echo -e `date +'%F %T'`'\t' `top | head -n3 | tail -n1 | tr -d ',' | awk '{print $2+$4}'` >> cpu_util_$D.txt
-	ls -Rrt > file.txt
-	grep 'cpu_util_' file.txt | while read- r line; do
+	find cpu_util_* > file.txt | sed 's/ /\n/g'
+	while read- r line; do
 		Time=$(echo $line | head -c 22 | tail -c 13)
 		DP=$(echo $line | head -c 19 | tail -c 10)
 		diff=$(($(($(date -d $DN +%s) - $(date -d $DP +%s)))/3600))
